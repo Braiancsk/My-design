2977cfdf4ac56798bbf51238bf8749fbd1258dce
Braian Fellipe <74879806+Braiancsk@users.noreply.github.com>
2021-07-09 17:28:53 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const burger = document.querySelector('.hamburger')
+const nav = document.querySelector('header ul');
+
+burger.addEventListener('click',()=>{
+  burger.classList.toggle('is-active');
+});
+
+burger.addEventListener('click',()=>{
+    nav.classList.toggle('nav-active');
+});
+
+
+
+
+ 
+
+ 
+
+
+
